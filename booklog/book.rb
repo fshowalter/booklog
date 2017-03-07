49b738419b18aa52db575fbbcd7b3f536fff826a
Frank Showalter <fshowalter@gmail.com>
2017-03-06 20:55:37 -0500

    add authors/books/review

--- booklog/book.rb
+++ booklog/book.rb
@@ -5,9 +5,9 @@ module Booklog
   # Responsible for holding book data.
   #
   class Book
-    attr_reader :id, :title, :title_with_author, :isbn, :sortable_title, :aka_titles, :year_published, :authors
+    attr_reader :id, :title, :title_with_author, :isbn, :sortable_title, :aka_titles, :year_published, :authors, :publisher, :pages_total
 
-    def initialize(id:, title:, aka_titles:, isbn:, authors:, year_published:, sortable_title:)
+    def initialize(id:, title:, aka_titles:, isbn:, authors:, year_published:, sortable_title:, publisher: nil, pages_total: 0)
       @id = id
       @title = title
       @title_with_author = "#{title} by #{authors.map(&:name).to_sentence}"
@@ -16,6 +16,8 @@ module Booklog
       @authors = authors
       @isbn = isbn
       @sortable_title = sortable_title
+      @publisher = publisher
+      @pages_total = pages_total
     end
   end
 end
--- booklog/review.rb
+++ booklog/review.rb
@@ -6,9 +6,9 @@ module Booklog
   class Review
     extend Forwardable
 
-    attr_reader :sequence, :date, :grade, :content, :cover, :cover_placeholder
+    attr_reader :sequence, :date, :grade, :content, :cover, :cover_placeholder, :cover_ratio
 
-    def initialize(sequence:, book:, date:, grade:, content:, cover: '', cover_placeholder: nil)
+    def initialize(sequence:, book:, date:, grade:, content:, cover: '', cover_placeholder: nil, cover_ratio: 1)
       @book = book
       @sequence = sequence
       @date = date
@@ -16,9 +16,10 @@ module Booklog
       @content = content
       @cover = cover
       @cover_placeholder = cover_placeholder
+      @cover_ratio = cover_ratio
     end
 
     def_delegators :@book, :id, :title, :sortable_title, :aka_titles, :title_with_author, :authors, :year_published,
-                   :isbn
+                   :isbn, :publisher, :pages_total
   end
 end
