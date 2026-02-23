9c11a7661b3c92e383a2d16e5a1919f8000e8368
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2026-02-22 19:53:09 -0500

    refactor: simplify reviewed works in author exports to use slugs instead of full objects (#1022)
    * refactor: simplify reviewed works in author exports to use slugs instead of full objects
    
    * format
    
    * snaps

--- booklog/exports/json_author_with_reviewed_works.py
+++ booklog/exports/json_author_with_reviewed_works.py
@@ -1,6 +1,5 @@
 from booklog.exports.json_author import JsonAuthor
-from booklog.exports.json_reviewed_work import JsonReviewedWork
 
 
 class JsonAuthorWithReviewedWorks(JsonAuthor):
-    reviewedWorks: list[JsonReviewedWork]
+    reviewedWorks: list[str]
